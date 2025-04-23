ceb4bdb813057908896938f1c3bca50d34ab5fa5
Johannes Lingk <johannes.lingk@gmail.com>
2025-04-23 11:36:42 +0200

    added screen placeholders

---
+++ lib/screens/title_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TitleScreen extends StatelessWidget {
+  const TitleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
